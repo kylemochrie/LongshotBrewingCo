3c4ee0fd46307a693d677b44571e6c680cacb209
Kyle Mochrie <kbm5683@rit.edu>
2015-12-08 16:03:01 -0500

    Add more text

--- index.php
+++ index.php
@@ -29,7 +29,10 @@ include "header.html";
     <div id = "intro">
         <h1>Welcome</h1>
         <p>Enjoy the delicious food and beer offered at Longshot Brewery Company! Open to all ages.
-            We bring together music, sports, food, and of course – beer. </p>
+            We bring together music, sports, food, and of course – <span id="beer">beer</span>. We offer 52 different kinds of beers
+            and look forward to you trying them. We also provide a guided tour of our brewery, which is fun and
+            informative for kids and adults. So what are you waiting for, come and check us out!
+        </p>
     </div>
 
     <div class="slideshow-container">
