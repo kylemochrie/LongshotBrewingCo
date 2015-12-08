3e6b6114fcdb067911e761b769c931fe528413b4
Nick Rung <nrung@optonline.com>
2015-12-08 16:03:50 -0500

    Carousel fix

--- index.php
+++ index.php
@@ -1,29 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-include "header.html";
-?>
+<?php include "header.html"; ?>
 <body>
 <div class="container-fluid">
-<div id ="top">
-    <a href="#"><img src="img/fb-logo.png" alt="Facebook Logo"></a>
-    <a href="#"><img src="img/ig-logo.png" alt="Instagram Logo"></a>
-    <a href="#"><img src="img/twitter-logo.png" alt="twitter logo"></a>
-</div>
-    <div id="jumbo">
-        <img id="logo" class="center-block" src="img/longshot-logo.png" alt="Longshot Brewing Logo">
-    </div>
-        <nav>
-        <ul class="nav nav-tabs nav-justified">
-            <li role="presentation"><a href="about.php">About Us</a></li>
-            <li role="presentation"><a href="beers.php">Beers</a></li>
-            <li role="presentation"><a href="menu.php">Menu</a></li>
-            <li role="presentation"><a href="tours.php">Tours</a></li>
-            <li role="presentation"><a href="events.php">Events</a></li>
-            <li role="presentation"><a href="store.php">Store</a></li>
-            <li role="presentation"><a href="contact.php">Contact Us</a></li>
-        </ul>
-    </nav>
+    <?php include "nav.html"; ?>
     <div class="row"></div>
 
     <div id = "intro">
@@ -36,11 +16,14 @@ include "header.html";
     </div>
 
     <div class="slideshow-container">
-        <?php
-        include "car.html";
-        ?>
+        <?php include "car.html"; ?>
+    </div>
+
+    <div id="intro">
+        <p>Enjoy the delicious food and beer offered at Longshot Brewery Company! Open to all ages.
+            We bring together music, sports, food, and of course – beer. </p>
     </div>
 </div>
-
+<?php include "footer.html"; ?>
 </body>
 </html>
