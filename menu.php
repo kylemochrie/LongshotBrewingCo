8c31374e90faeb44cf73932868be32476408fdd3
cmz6274 <cmz6274@rit.edu>
2015-12-10 11:27:12 -0500

    Added content to menu page

---
+++ menu.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "header.html"; ?>
+  <body>
+    <div class="container-fluid">
+        <?php include "nav.html"; ?>
+        <div class="row"></div>
+    
+        <div>
+            <h1>Our Menu</h1>
+            <div>
+              <h2>Appetizers</h2>
+              <ul>
+                <li>Onion Rings</li>
+                <li>Mozzarella Sticks</li>
+                <li>Hot Pepper Cheese Bites</li>
+                <li>Oven Roasted Parmesan Zucchini Sticks</li>
+                <li>Pizza Roasted Chickpeas</li>
+                <li>Homemade Kettle Chips w/ Spinach Artichoke Dip</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Sides</h2>
+              <ul>
+                <li>Fries</li>
+                <li>Mashed Potatoes</li>
+                <li>Side Salad</li>
+                <li>Fresh Corn</li>
+                <li>Roasted Vegetables</li>
+                <li>Coleslaw</li>
+                <li>Fruit Salad</li>
+                <li>Creamy Mac & Cheese</li>
+                <li>Gruyere, Prosciutto, Green Onion Scones</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Soups</h2>
+              <ul>
+                <li>French Onion</li>
+                <li>Homemade Red Bean Chili</li>
+                <li>Seven Vegetable Minestrone</li>
+                <li>Chicken Tortilla</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Salads</h2>
+              <ul>
+                <li>Caesar Salad</li>
+                <li>Greek Salad</li>
+                <li>Grilled Chicken Salad</li>
+                <li>Buffalo Chicken Salad</li>
+                <li>Taco Salad</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Sandwiches</h2>
+              <ul>
+                <li>Turkey Club</li>
+                <li>BLT</li>
+                <li>Veggie Wrap</li>
+                <li>Reuben</li>
+                <li>French Dip</li>
+                <li>Pulled Pork Po' Boy</li>
+                <li>Hot Turkey Sandwich</li>
+                <li>Gourmet Grilled Cheese Sandwich</li>
+                <li>Breaded Whitefish Sandwich</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Burgers</h2>
+              <ul>
+                <li>Black Angus Burger</li>
+                <li>Bacon Cheddar Burger</li>
+                <li>Mushroom Onion Burger</li>
+                <li>Turkey Burger</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Entrees</h2>
+              <ul>
+                <li>Mushroom Filled Pork Tenderloin</li>
+                <li>Smothered Chicken</li>
+                <li>Beef Stroganoff</li>
+                <li>Chicken Mozzarella Pasta w/ Sun Dried Tomatoes</li>
+                <li>Stuffed Portabella Mushrooms</li>
+                <li>Lime Cilantro Chicken with Avocado</li>
+                <li>Grilled Salmon</li>
+                <li>Lemon Pepper Tilapia</li>
+                <li>Crab Cakes</li>
+              </ul>
+            </div>
+            
+            <div>
+              <h2>Desserts</h2>
+              <ul>
+                <li>Chocolate Mousse Layer Cake</li>
+                <li>Lava Cake</li>
+                <li>Vanilla Bean Cake</li>
+                <li>Custard Cake</li>
+                <li>Churro Bites</li>
+                <li>Appledoodles</li>
+                <li>Berry Crumble</li>
+              </ul>
+            </div>
+            
+        </div>
+    </div>
+    <?php include "footer.html"; ?>
+  </body>
+</html>
