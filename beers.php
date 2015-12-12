c6be90938c492257c159f2b5e00705ec2721d8e8
Nick Rung <nrung@optonline.com>
2015-12-12 18:47:10 -0500

    Beers page finished

--- beers.php
+++ beers.php
@@ -5,18 +5,80 @@
 <?php include "nav.html"; ?>
 <div class="container-fluid">
 
-    <div class="row">
-        <h1>Our Beers</h1>
+    <div class="row" id="intro"><h1>Our Beers</h1></div>
 
-        <div class="col-md-3"></div>
-        <div class="col-md-3"></div>
-        <div class="col-md-3"></div>
+    <div class="row" id="intro">
+        <div class="row" id="intro">
+            <h2>Year-Round</h2>
+            <hr>
+        </div>
     </div>
 
-    <div class="row">
-        <div class="col-md-3"></div>
-        <div class="col-md-3"></div>
-        <div class="col-md-3"></div>
+    <div class="row" id="intro">
+        <div class="col-sm-3">
+            <div class="caption text-center">Annual Ale</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/annual.png" alt="Annual Ale"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Hella Bitter IPA</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/bitter.png" alt="Hella Bitter IPA"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Double Bock</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/double.png" alt="Double Bock"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Scotch Ale</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/scotch.png" alt="Scotch Ale"></div>
+    </div>
+
+    <div class="row" id="intro">
+        <div class="col-sm-3">
+            <div class="caption text-center">Hometown Ale</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/hometown.png" alt="Hometown Ale"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Into the Mystic IPA</div>
+            <img class="img-thumbnail img-responsive center-block" src="img/beer/mystic.png" alt="Into the Mystic IPA">
+        </div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Richard's Irish Ale</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/irish.png" alt="Richard's Irish Ale"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Tall Dark and Handsome Stout</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/tallDark.png" alt="Tall Dark and Handsome Stout">
+        </div>
+    </div>
+
+    <div class="row" id="intro">
+        <h2>Seasonal</h2>
+        <hr>
+    </div>
+
+    <div class="row" id="intro">
+        <div class="col-sm-3">
+            <div class="caption text-center">Oktoberfest (Fall)</div>
+            <img class="img-thumbnail img-responsive center-block" src="img/beer/oktoberfest.png" alt="Oktoberfest">
+        </div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Pumpkin Ale (Fall)</div>
+            <img class="img-thumbnail img-responsive center-block" src="img/beer/pumpkinAle.png" alt="Pumpkin Ale">
+        </div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Winter Lager (Winter)</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/winterLager.png" alt="Winter Lager"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Spice Ale (Winter)</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/spice.png" alt="Spice Ale"></div>
+    </div>
+
+    <div class="row" id="intro">
+        <div class="col-sm-3">
+            <div class="caption text-center">Peach Ale (Spring)</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/peach.png" alt="Peach Ale"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Summer Ale (Summer)</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/summerAle.png" alt="Summer Ale"></div>
+        <div class="col-sm-3">
+            <div class="caption text-center">Honey Wheat Ale (Summer)</div>
+            <img class="img-thumbnail img-responsive" src="img/beer/honeyWheat.png" alt="Honey Wheat Ale"></div>
+        <div class="col-sm-3"></div>
     </div>
 
 </div>
