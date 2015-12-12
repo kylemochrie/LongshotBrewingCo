4ed5ea2122ff9571aa2229af515f92397900125f
Kyle Mochrie <kbm5683@rit.edu>
2015-12-12 14:01:22 -0500

    Update store and add thank you page.

--- store.php
+++ store.php
@@ -9,83 +9,59 @@
         <h1>Shop</h1>
 
         <div class="row">
-            <div class="col-md-3">
-                <img src="" alt="Men's Black T-shirt" title="Men's Black T-shirt"/>
+            <div class="col-md-4">
+                <img class="store" src="img/mensblacktshirt.png" alt="Men's Black T-shirt" title="Men's Black T-shirt"/>
                 <p>Men's Black T-shirt - $18</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-            <div class="col-md-3">
-                <img src="" alt="Men's White T-shirt" title="Men's White T-shirt"/>
+            <div class="col-md-4">
+                <img class="store" src="img/menswhitetshirt.png" alt="Men's White T-shirt" title="Men's White T-shirt"/>
                 <p>Men's White T-shirt - $18</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-            <div class="col-md-3">
-                <img src="" alt="Women's Gray T-shirt" title="Women's Gray T-shirt"/>
+            <div class="col-md-4">
+                <img class="store" src="img/womensgraytshirt.png" alt="Women's Gray T-shirt" title="Women's Gray T-shirt"/>
                 <p>Women's Gray T-shirt - $18</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-            <div class="col-md-3">
-                <img src="" alt="Women's White T-shirt" title="Women's White T-shirt"/>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img class="store" src="img/womenswhitetshirt.png" alt="Women's White T-shirt" title="Women's White T-shirt"/>
                 <p>Women's White T-shirt - $18</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-md-3">
-                <img src="" alt="Trucker Hat" title="Trucker Hat"/>
-                <p>Trucker Hat - $15</p>
-            </div>
-            <div class="col-md-3">
-                <img src="" alt="Necktie" title="Necktie"/>
+            <div class="col-md-4">
+                <img class="store" src="img/tie.png" alt="Necktie" title="Necktie"/>
                 <p>Necktie - $15</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-            <div class="col-md-3">
-                <img src="" alt="Glass Tankard" title="Glass Tankard"/>
-                <p>Glass Tankard - $7</p>
-            </div>
-            <div class="col-md-3">
-                <img src="" alt="Shot Glass" title="Shot Glass"/>
-                <p>Shot Glass - $6</p>
-            </div>
-        </div>
-        
-        <div class="row">
-            <div class="col-md-3">
-                <img src="" alt="Black Koozie" title="Black Koozie"/>
+            <div class="col-md-4">
+                <img class="store" src="img/blackKoozie.png" alt="Black Koozie" title="Black Koozie"/>
                 <p>Black Koozie - $5</p>
-            </div>
-            <div class="col-md-3">
-                <img src="" alt="6-Piece Coaster Set" title="6-Piece Coaster Set"/>
-                <p>6-Piece Coaster Set - $7</p>
-            </div>
-            <div class="col-md-3">
-                <img src="" alt="Longshot Logo Magnet" title="Longshot Logo Magnet"/>
-                <p>Longshot Logo Magnet - $4</p>
-            </div>
-            <div class="col-md-3">
-                <img src="" alt="Longshot Motto Magnet" title="Longshot Motto Magnet"/>
-                <p>Longshot Motto Magnet - $4</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
         </div>
         
         <div class="row">
-            <div class="col-md-3">
-                <img src="" alt="Heart Keychain" title="Heart Keychain"/>
-                <p>Heart Keychain - $6</p>
+            <div class="col-md-4">
+                <img class="store" src="img/magnet.png" alt="Longshot Motto Magnet" title="Longshot Motto Magnet"/>
+                <p>Longshot Motto Magnet - $4</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-            <div class="col-md-3">
-                <img src="" alt="Bumper Sticker" title="Bumper Sticker"/>
+            <div class="col-md-4">
+                <img class="store" src="img/bumperSticker.png" alt="Bumper Sticker" title="Bumper Sticker"/>
                 <p>Bumper Sticker - $3</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
-            <div class="col-md-3">
-                <img src="" alt="Ping Pong Ball" title="Ping Pong Ball"/>
-                <p>Ping Pong Ball - $3</p>
-            </div>
-            <div class="col-md-3">
-                <img src="" alt="Cheers Ornament" title="Cheers Ornament"/>
-                <p>Cheers Ornament - $10</p>
+            <div class="col-md-4">
+                <img class="store" src="img/heartKeychain.png" alt="Heart Keychain" title="Heart Keychain"/>
+                <p>Heart Keychain - $6</p>
+                <a href="thankyou.php" class="btn btn-primary btn-lg" role="button">Buy</a>
             </div>
         </div>
-        
     </div>
-
 </div>
 <?php include "footer.html"; ?>
 </body>
---
+++ thankyou.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include "header.html"; ?>
+<body>
+<?php include "nav.html"; ?>
+<div class="container-fluid">
+
+    <div class="row" id="intro">
+        <h2>Thank you for your order!</h2><br>
+        <a href="store.php" class="btn btn-primary btn-lg" role="button">Back to Store</a>
+    </div>
+
+</div>
+<?php include "footer.html"; ?>
+</body>
+</html>
