51ef771e7a72b75224de6c76aa6d5cd15602940d
Nick Rung <nrung@optonline.com>
2015-12-11 19:24:35 -0500

    Changed some paragraphs

--- about.php
+++ about.php
@@ -6,19 +6,27 @@
 <div class="container-fluid">
 
     <div class="row" id="intro">
-        <h1>About Us</h1>
 
-        <p>It all started on a farm in Kansas. After the great drought of 1933, Richard Hanson sold the family farm and
-            moved east to Rochester in upstate, NY. After struggling for work he accepted an apprenticeship from his
+        <div class="col-md-6">
+            <h1>About Us</h1>
+
+            <p>It all started on a farm in Kansas. After the great drought of 1933, Richard Hanson and his mother Verna
+                sold the
+                family farm and moved east to Rochester in upstate, NY. After struggling for work he became an
+                apprentice to his
             uncle, Michael Hanson. Through his apprenticeship Richard learned everything he had to know about the
-            brewing
-            process and discovered a new passion. After Michael passed away of old age he left the brewery to Richard
-            who
-            then proceeded to expand distribution along the east coast and made the brewery more prosperous than his
-            uncle
-            ever imagined. Today Longshot Brewery has changed a lot since its inception, but the same family-owned touch
-            still remains.
+                brewing process and discovered a new passion. Years passed along with Verna and Michael, who left the
+                brewery in
+                Richard's capable hands. Richard then proceeded to expand distribution along the east coast and made the
+                brewery
+                more prosperous than his uncle ever imagined. Today Longshot Brewery has changed a lot since its
+                inception, but
+                the same family-owned touch still remains.
         </p>
+        </div>
+        <div class="col-md-6">
+            <img class="img-rounded img-responsive" src="img/old-woman-farm.jpg" alt="Old woman on farm">
+        </div>
     </div>
 
 </div>
