cfe7378eb9a74161edc9769460eaf2b0a1d5922c
cmz6274 <cmz6274@rit.edu>
2015-12-12 11:13:53 -0500

    Added store content
    I left out the images again because they may also need to be resized

--- store.php
+++ store.php
@@ -9,10 +9,81 @@
         <h1>Shop</h1>
 
         <div class="row">
-
-            <div class="col-md-3"></div>
-            Merchendise here in grid form.
+            <div class="col-md-3">
+                <img src="" alt="Men's Black T-shirt" title="Men's Black T-shirt"/>
+                <p>Men's Black T-shirt - $18</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Men's White T-shirt" title="Men's White T-shirt"/>
+                <p>Men's White T-shirt - $18</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Women's Gray T-shirt" title="Women's Gray T-shirt"/>
+                <p>Women's Gray T-shirt - $18</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Women's White T-shirt" title="Women's White T-shirt"/>
+                <p>Women's White T-shirt - $18</p>
+            </div>
         </div>
+        
+        <div class="row">
+            <div class="col-md-3">
+                <img src="" alt="Trucker Hat" title="Trucker Hat"/>
+                <p>Trucker Hat - $15</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Necktie" title="Necktie"/>
+                <p>Necktie - $15</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Glass Tankard" title="Glass Tankard"/>
+                <p>Glass Tankard - $7</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Shot Glass" title="Shot Glass"/>
+                <p>Shot Glass - $6</p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-3">
+                <img src="" alt="Black Koozie" title="Black Koozie"/>
+                <p>Black Koozie - $5</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="6-Piece Coaster Set" title="6-Piece Coaster Set"/>
+                <p>6-Piece Coaster Set - $7</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Longshot Logo Magnet" title="Longshot Logo Magnet"/>
+                <p>Longshot Logo Magnet - $4</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Longshot Motto Magnet" title="Longshot Motto Magnet"/>
+                <p>Longshot Motto Magnet - $4</p>
+            </div>
+        </div>
+        
+        <div class="row">
+            <div class="col-md-3">
+                <img src="" alt="Heart Keychain" title="Heart Keychain"/>
+                <p>Heart Keychain - $6</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Bumper Sticker" title="Bumper Sticker"/>
+                <p>Bumper Sticker - $3</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Ping Pong Ball" title="Ping Pong Ball"/>
+                <p>Ping Pong Ball - $3</p>
+            </div>
+            <div class="col-md-3">
+                <img src="" alt="Cheers Ornament" title="Cheers Ornament"/>
+                <p>Cheers Ornament - $10</p>
+            </div>
+        </div>
+        
     </div>
 
 </div>
